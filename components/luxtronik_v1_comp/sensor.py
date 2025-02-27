926310f5d3002a8167a044cadc0c2ad443cff0ec
Christian Brosius <Christian.Brosius@gmail.com>
2025-02-27 13:55:52 +0100

    another try with sonnet ai

--- components/luxtronik_v1_comp/sensor.py
+++ components/luxtronik_v1_comp/sensor.py
@@ -19,35 +19,35 @@ CONF_UART_ID = "uart_id"
 CONF_TEMP_VL = "temp_VL"
 CONF_TEMP_RL = "temp_RL"
 
-
-def luxtronik_v1_comp_schema():
-    return sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, accuracy_decimals=1, device_class=DEVICE_CLASS_TEMPERATURE, state_class=STATE_CLASS_MEASUREMENT)
-
-# Component schema
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
         cv.GenerateID(): cv.declare_id(LuxtronikV1Component),
-        cv.Optional(CONF_TEMP_VL): luxtronik_v1_comp_schema(),
-        cv.Optional(CONF_TEMP_RL): luxtronik_v1_comp_schema(),
+        cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.Optional(CONF_TEMP_VL): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_TEMP_RL): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
     }
 ).extend(cv.polling_component_schema("10s"))
 
-
 async def to_code(config):
-    # Create the object.
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    # Get the uart component.
+    
     uart_comp = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart(uart_comp))
 
-    # Temperature Sensors
     if CONF_TEMP_VL in config:
-        conf = config[CONF_TEMP_VL]
-        sens = await sensor.new_sensor(conf)
+        sens = await sensor.new_sensor(config[CONF_TEMP_VL])
         cg.add(var.set_temp_VL(sens))
     if CONF_TEMP_RL in config:
-        conf = config[CONF_TEMP_RL]
-        sens = await sensor.new_sensor(conf)
+        sens = await sensor.new_sensor(config[CONF_TEMP_RL])
         cg.add(var.set_temp_RL(sens))
