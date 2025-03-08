97ca218300c9d790be57196aab57ab4cd605f1e9
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-08 21:51:30 +0100

    fix last commit

--- components/luxtronik_v1/sensor.cpp
+++ components/luxtronik_v1/sensor.cpp
@@ -22,7 +22,6 @@ void LuxtronikV1Sensor::setup() {
 }
 
 void LuxtronikV1Sensor::loop() {
-<<<<<<< HEAD
   uint32_t now = millis();
   if (now - this->last_loop_ms_ < 5000) {
     // Nur alle 5 Sekunden ausführen.
@@ -30,8 +29,6 @@ void LuxtronikV1Sensor::loop() {
   }
   this->last_loop_ms_ = now;
 
-=======
->>>>>>> parent of 86b8421 (only check uart every 5 seconds)
   if (this->uart_ == nullptr) {
     ESP_LOGW(TAG, "Loop() - UART component not set in loop()");
     return;
