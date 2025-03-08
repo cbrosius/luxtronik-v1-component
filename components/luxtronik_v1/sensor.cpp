d15a9559a0405bec82832776bdcede57adb6fa03
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-08 17:01:56 +0100

    add uart-check in Loop()

--- components/luxtronik_v1/sensor.cpp
+++ components/luxtronik_v1/sensor.cpp
@@ -15,6 +15,10 @@ LuxtronikV1Sensor::LuxtronikV1Sensor() : PollingComponent(60000) {}
 void LuxtronikV1Sensor::set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
 
 void LuxtronikV1Sensor::loop() {
+  if (!this->available()) {
+    ESP_LOGW(TAG, "UART not available in loop()");
+    return;
+  }
   // Read message
   while (this->available()) {
     uint8_t byte;
