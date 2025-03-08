51c0be7374915728e36bbd8536de456ee116cc09
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-08 17:41:30 +0100

    remove debug code from init.py

--- components/luxtronik_v1/__init__.py
+++ components/luxtronik_v1/__init__.py
@@ -27,8 +27,6 @@ async def to_code(config):
     await cg.register_component(var, config)
     
     uart_component = await cg.get_variable(config[CONF_UART_ID])
-    # Inject a raw C++ comment (note: the pointer value won't be resolved here)
-    cg.add(cg.raw_cpp('// luxtronik_v1: Retrieved UART pointer'))
     
     cg.add(var.set_uart(uart_component))
     await uart.register_uart_device(var, config)
