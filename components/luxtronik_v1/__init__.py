6ffdcf5c75ac505252e71488b5f1da67f1d86212
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-08 13:46:37 +0100

    try fixing registration

--- components/luxtronik_v1/__init__.py
+++ components/luxtronik_v1/__init__.py
@@ -15,18 +15,18 @@ CONF_LUXTRONIK_V1_ID = "luxtronik_v1_id"
 CONF_TEMPERATURE_SENSORS = "temperature_sensors"
 
 luxtronik_v1_ns = cg.esphome_ns.namespace('luxtronik_v1')
-Luxtronik_v1_Component = luxtronik_v1_ns.class_('luxtronik_v1_sensor', cg.PollingComponent, uart.UARTDevice)
-
-TEMPERATURE_SENSOR_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_CELSIUS,
-    accuracy_decimals=1,
-    device_class=DEVICE_CLASS_TEMPERATURE,
-    state_class=STATE_CLASS_MEASUREMENT
-)
+LuxtronikV1Component = luxtronik_v1_ns.class_('luxtronik_v1_sensor', cg.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Luxtronik_v1_Component),
-    cv.Optional(CONF_TEMPERATURE_SENSORS): cv.ensure_list(TEMPERATURE_SENSOR_SCHEMA),
+    cv.GenerateID(): cv.declare_id(LuxtronikV1Component),
+    cv.Optional(CONF_TEMPERATURE_SENSORS): cv.ensure_list(
+        sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT
+        )
+    ),
 }).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
@@ -35,6 +35,6 @@ async def to_code(config):
     await uart.register_uart_device(var, config)
 
     if CONF_TEMPERATURE_SENSORS in config:
-        for i, conf in enumerate(config[CONF_TEMPERATURE_SENSORS]):
+        for conf in config[CONF_TEMPERATURE_SENSORS]:
             sens = await sensor.new_sensor(conf)
-            cg.add(var.set_temperature_sensor(i, sens))
+            cg.add(var.set_temperature_sensor(sens))
--- components/luxtronik_v1/sensor.py
+++ components/luxtronik_v1/sensor.py
@@ -6,21 +6,18 @@ from esphome.const import (
     STATE_CLASS_MEASUREMENT,
     UNIT_CELSIUS,
 )
-from . import luxtronik_v1_ns, Luxtronik_v1_Component, CONF_TEMPERATURE_SENSORS
+from . import luxtronik_v1_ns, LuxtronikV1Component
 
-LuxtronikV1Sensor = luxtronik_v1_ns.class_("luxtronik_v1_sensor", sensor.Sensor)
+CONF_TEMPERATURE_SENSORS = "temperature_sensors"
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement=UNIT_CELSIUS,
     accuracy_decimals=1,
     device_class=DEVICE_CLASS_TEMPERATURE,
     state_class=STATE_CLASS_MEASUREMENT,
-).extend({
-    cv.GenerateID(): cv.declare_id(LuxtronikV1Sensor),
-})
+)
 
 async def to_code(config):
-    if CONF_TEMPERATURE_SENSORS in config:
-        for conf in config[CONF_TEMPERATURE_SENSORS]:
-            var = await sensor.new_sensor(conf)
-            await cg.register_component(var, config)
+    paren = await cg.get_variable(config[CONF_TEMPERATURE_SENSORS])
+    var = await sensor.new_sensor(config)
+    cg.add(paren.set_temperature_sensor(var))
