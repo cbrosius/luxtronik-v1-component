10adadb4cedbbcdb84ab5f3d20569bc2ba786002
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-09 11:48:44 +0100

    add variables and constants

--- components/luxtronik_v1_component/luxtronik_v1_component.h
+++ components/luxtronik_v1_component/luxtronik_v1_component.h
@@ -6,13 +6,25 @@
 namespace esphome {
 namespace luxtronik_v1_component {
 
-class LuxtronikV1Component : public uart::UARTDevice, public Component {
-  public:
-    void setup() override;
-    void loop() override;
-    void dump_config() override;
-};
+static const char ASCII_CR = '\r';
+static const char ASCII_LF = '\n';
+static const uint8_t READ_BUFFER_LENGTH = 255;
 
+class LuxtronikV1Component : public uart::UARTDevice, public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+
+  void set_uart_parent(uart::UARTComponent *parent) { 
+    this->parent_ = parent;
+  }
+
+ protected:
+  uart::UARTComponent *parent_{nullptr};
+  char read_buffer_[READ_BUFFER_LENGTH];
+  size_t read_pos_{0};
+};
 
 }  // namespace luxtronik_v1_component
 }  // namespace esphome
