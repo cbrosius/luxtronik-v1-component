1ea073fbb52a25d85130fbe61ad1566b1d09ab12
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-09 08:54:15 +0100

    fix3

--- components/luxtronik_v1_minimal/luxtronik_v1_minimal.cpp
+++ components/luxtronik_v1_minimal/luxtronik_v1_minimal.cpp
@@ -7,7 +7,7 @@ namespace luxtronik_v1_minimal {
 static const char *TAG = "luxtronik_v1_minimal.sensor";
 
 void LuxtronikV1MinimalSensor::setup() {
-
+    ESP_LOGCONFIG(TAG, "Setting up Luxtronik V1 Minimal...");
 }
 
 void LuxtronikV1MinimalSensor::update() {
@@ -16,11 +16,15 @@ void LuxtronikV1MinimalSensor::update() {
 }
 
 void LuxtronikV1MinimalSensor::loop() {
-
+    const uint32_t now = millis();
+    if (now - this->last_read_ < 5000) {
+        return;
+    }
+    this->last_read_ = now;
 }
 
-void LuxtronikV1MinimalSensor::dump_config(){
-    ESP_LOGCONFIG(TAG, "LuxtronikV1MinimalSensor");
+void LuxtronikV1MinimalSensor::dump_config() {
+    ESP_LOGCONFIG(TAG, "LuxtronikV1MinimalSensor:");
 }
 
 void LuxtronikV1MinimalSensor::send_cmd(std::string message) {
--- components/luxtronik_v1_minimal/luxtronik_v1_minimal.h
+++ components/luxtronik_v1_minimal/luxtronik_v1_minimal.h
@@ -3,18 +3,24 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace luxtronik_v1_minimal {
 
 class LuxtronikV1MinimalSensor : public sensor::Sensor, public PollingComponent, public uart::UARTDevice {
  public:
+  // Constructor with 60s polling interval
+  LuxtronikV1MinimalSensor() : PollingComponent(5000) {}
+
   void setup() override;
   void update() override;
   void loop() override;
   void dump_config() override;
+
  protected:
   void send_cmd(std::string message);
+  uint32_t last_read_{0};
 };
 
 }  // namespace luxtronik_v1_minimal
