4837fa19bb10b5122d233d21e3d28a51c4924095
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-09 09:02:31 +0100

    fix6

--- components/luxtronik_v1_minimal/luxtronik_v1_minimal.cpp
+++ components/luxtronik_v1_minimal/luxtronik_v1_minimal.cpp
@@ -40,7 +40,7 @@ void LuxtronikV1MinimalSensor::send_cmd(std::string message) {
         return;
     }
     
-    ESP_LOGV(TAG, "Sending command: %s", message.c_str());
+    ESP_LOGW(TAG, "Sending command: %s", message.c_str());
     message += "\r\n";
     this->parent_->write_str(message.c_str());
 }
