ae8050355a276da125003c1c9ae54fce1c3c9b7c
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-09 11:16:32 +0100

    create luxtronik_v1_temperatures template

---
+++ components/luxtronik_v1_temperatures/luxtronik_v1_Temperatures.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace luxtronik_v1_minimal {
+
+static const uint8_t READ_BUFFER_LENGTH = 255;
+static const char ASCII_CR = '\r';
+static const char ASCII_LF = '\n';
+
+class LuxtronikV1TemperaturesSensor : public sensor::Sensor, public PollingComponent, public uart::UARTDevice {
+ public:
+  // Constructor with polling interval
+  LuxtronikV1TemperaturesSensor() : PollingComponent() {}
+
+  void set_uart_parent(uart::UARTComponent *parent) { 
+    this->parent_ = parent;
+  }
+
+  void setup() override;
+  void update() override;
+  void loop() override;
+  void dump_config() override;
+
+ protected:
+  void send_cmd(std::string message);
+  
+  // Buffer management
+  char read_buffer_[READ_BUFFER_LENGTH];
+  size_t read_pos_{0};
+  
+  // UART parent
+  uart::UARTComponent *parent_{nullptr};
+};
+
+}  // namespace luxtronik_v1_minimal
+}  // namespace esphome
---
+++ components/luxtronik_v1_temperatures/luxtronik_v1_temperatures.cpp
@@ -0,0 +1,67 @@
+#include "esphome/core/log.h"
+#include "luxtronik_v1_temperature.h"
+
+namespace esphome {
+namespace luxtronik_v1_temperature {
+
+static const char *TAG = "luxtronik_v1_temperature.sensor";
+
+void LuxtronikV1TemperaturesSensor::setup() {
+    ESP_LOGCONFIG(TAG, "Setting up Luxtronik V1 Minimal...");
+    if (this->parent_ == nullptr) {
+        ESP_LOGE(TAG, "UART parent not set!");
+        this->mark_failed();
+        return;
+    }
+}
+
+void LuxtronikV1TemperaturesSensor::update() {
+        send_cmd("1100");
+}
+
+void LuxtronikV1TemperaturesSensor::loop() {
+    if (this->parent_ == nullptr) {
+        ESP_LOGW(TAG, "Cannot loop - UART parent not set");
+        return;
+    }
+    
+    while (this->parent_->available()) {
+        uint8_t c;
+        if (!this->parent_->read_byte(&c)) {
+            continue;
+        }
+        
+        if (c == ASCII_CR || c == ASCII_LF) {
+            if (this->read_pos_ > 0) {
+                this->read_buffer_[this->read_pos_] = '\0';
+                ESP_LOGD(TAG, "Received: %s", this->read_buffer_);
+                this->read_pos_ = 0;
+            }
+        } else {
+            this->read_buffer_[this->read_pos_++] = c;
+            if (this->read_pos_ >= READ_BUFFER_LENGTH) {
+                this->read_pos_ = 0;
+            }
+        }
+    }
+}
+
+void LuxtronikV1TemperaturesSensor::dump_config() {
+    ESP_LOGCONFIG(TAG, "LuxtronikV1TemperaturesSensor:");
+    ESP_LOGCONFIG(TAG, "  UART Parent: %s", this->parent_ ? "Set" : "Not Set");
+}
+
+void LuxtronikV1TemperaturesSensor::send_cmd(std::string message) {
+    if (this->parent_ == nullptr) {
+        ESP_LOGW(TAG, "Cannot send command - UART parent not set");
+        return;
+    }
+    
+    ESP_LOGV(TAG, "Sending command: %s", message.c_str());
+    message += "\r\n";
+    this->parent_->write_str(message.c_str());
+}
+
+
+}  // namespace luxtronik_v1_temperature
+}  // namespace esphome
