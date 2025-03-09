dc93ade54f2029360e2d1e82a9cacd20cfe3edd1
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-09 11:46:58 +0100

    start implementing

--- components/luxtronik_v1_component/luxtronik_v1_component.cpp
+++ components/luxtronik_v1_component/luxtronik_v1_component.cpp
@@ -7,15 +7,44 @@ namespace luxtronik_v1_component {
 static const char *TAG = "luxtronik_v1_component.component";
 
 void LuxtronikV1Component::setup() {
-
+    ESP_LOGCONFIG(TAG, "Setting up Luxtronik V1 Component...");
+    if (this->parent_ == nullptr) {
+        ESP_LOGE(TAG, "UART parent not set!");
+        this->mark_failed();
+        return;
+    }
 }
 
 void LuxtronikV1Component::loop() {
-
+    if (this->parent_ == nullptr) {
+        ESP_LOGW(TAG, "Cannot loop - UART parent not set");
+        return;
+    }
+    
+    while (this->parent_->available()) {
+        uint8_t c;
+        if (!this->parent_->read_byte(&c)) {
+            continue;
+        }
+        
+        if (c == ASCII_CR || c == ASCII_LF) {
+            if (this->read_pos_ > 0) {
+                this->read_buffer_[this->read_pos_] = '\0';
+                ESP_LOGD(TAG, "Received: %s", this->read_buffer_);
+                this->read_pos_ = 0;
+            }
+        } else {
+            this->read_buffer_[this->read_pos_++] = c;
+            if (this->read_pos_ >= READ_BUFFER_LENGTH) {
+                this->read_pos_ = 0;
+            }
+        }
+    }
 }
 
 void LuxtronikV1Component::dump_config(){
     ESP_LOGCONFIG(TAG, "Luxtronik_v1 component");
+    ESP_LOGCONFIG(TAG, "  UART Parent: %s", this->parent_ ? "Set" : "Not Set");
 }
 
 }  // namespace luxtronik_v1_component
