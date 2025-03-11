b940c539fcbfe44fe5433d84a2372302e6808421
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-11 12:07:56 +0100

    fix text_sensor import

--- components/luxtronik_v1_component/luxtronik_v1_component.h
+++ components/luxtronik_v1_component/luxtronik_v1_component.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/sensor/text_sensor.h"  // Make sure path is correct
 
 namespace esphome {
 namespace luxtronik_v1_component {
