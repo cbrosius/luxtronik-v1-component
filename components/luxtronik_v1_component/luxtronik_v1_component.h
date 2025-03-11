eeac1aeadc8ce65d14f22b5f3e17c800466ba410
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-11 18:31:11 +0100

    fix3

--- components/luxtronik_v1_component/luxtronik_v1_component.h
+++ components/luxtronik_v1_component/luxtronik_v1_component.h
@@ -75,22 +75,21 @@ class LuxtronikV1Component : public uart::UARTDevice, public PollingComponent {
   void set_status_letzter_start_sensor(text_sensor::TextSensor *sens) { status_letzter_start_ = sens; }
 
   // Add error sensor setters
-  void set_error1_fehlercode_numerisch_sensor(sensor::Sensor *sens) { error1_fehlercode_numerisch_ = sens; }
+  void set_error0_fehlercode_sensor(sensor::Sensor *sens) { error0_fehlercode_ = sens; }
+  void set_error0_fehlerbeschreibung_sensor(text_sensor::TextSensor *sens) { error0_fehlerbeschreibung_ = sens; }
+  void set_error0_zeitpunkt_sensor(text_sensor::TextSensor *sens) { error0_zeitpunkt_ = sens; }
+  void set_error1_fehlercode_sensor(sensor::Sensor *sens) { error1_fehlercode_ = sens; }
   void set_error1_fehlerbeschreibung_sensor(text_sensor::TextSensor *sens) { error1_fehlerbeschreibung_ = sens; }
   void set_error1_zeitpunkt_sensor(text_sensor::TextSensor *sens) { error1_zeitpunkt_ = sens; }
-  void set_error2_fehlercode_numerisch_sensor(sensor::Sensor *sens) { error2_fehlercode_numerisch_ = sens; }
+  void set_error2_fehlercode_sensor(sensor::Sensor *sens) { error2_fehlercode_ = sens; }
   void set_error2_fehlerbeschreibung_sensor(text_sensor::TextSensor *sens) { error2_fehlerbeschreibung_ = sens; }
   void set_error2_zeitpunkt_sensor(text_sensor::TextSensor *sens) { error2_zeitpunkt_ = sens; }
-  void set_error3_fehlercode_numerisch_sensor(sensor::Sensor *sens) { error3_fehlercode_numerisch_ = sens; }
+  void set_error3_fehlercode_sensor(sensor::Sensor *sens) { error3_fehlercode_ = sens; }
   void set_error3_fehlerbeschreibung_sensor(text_sensor::TextSensor *sens) { error3_fehlerbeschreibung_ = sens; }
   void set_error3_zeitpunkt_sensor(text_sensor::TextSensor *sens) { error3_zeitpunkt_ = sens; }
-  void set_error4_fehlercode_numerisch_sensor(sensor::Sensor *sens) { error4_fehlercode_numerisch_ = sens; }
+  void set_error4_fehlercode_sensor(sensor::Sensor *sens) { error4_fehlercode_ = sens; }
   void set_error4_fehlerbeschreibung_sensor(text_sensor::TextSensor *sens) { error4_fehlerbeschreibung_ = sens; }
   void set_error4_zeitpunkt_sensor(text_sensor::TextSensor *sens) { error4_zeitpunkt_ = sens; }
-  void set_error5_fehlercode_numerisch_sensor(sensor::Sensor *sens) { error5_fehlercode_numerisch_ = sens; }
-  void set_error5_fehlerbeschreibung_sensor(text_sensor::TextSensor *sens) { error5_fehlerbeschreibung_ = sens; }
-  void set_error5_zeitpunkt_sensor(text_sensor::TextSensor *sens) { error5_zeitpunkt_ = sens; }
-
 
   protected:
   float get_float_temp_(const std::string& value) { return std::atof(value.c_str()) / 10.0f; }
