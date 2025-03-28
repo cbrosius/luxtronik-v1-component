0528429bbff3525922dc2b51c4d3e27cd589a7fc
Christian Brosius <Christian.Brosius@gmail.com>
2025-03-28 17:38:04 +0100

    fix 1

--- components/luxtronik_v1_component/luxtronik_v1_component.cpp
+++ components/luxtronik_v1_component/luxtronik_v1_component.cpp
@@ -342,7 +342,7 @@ void LuxtronikV1Component::parse_modus_brauchwasser_message_(const char* message
         if (modus_brauchwasser_numerisch_ != nullptr) {
             publish_state_deferred_(modus_brauchwasser_numerisch_, val, "Mode", "Brauchwasser Numerisch");
         }
-        if (modus_brauchwasser_ != nullptr) {
+        if (modus_brauchwasser_ != nullptr && value != modus_brauchwasser_->get_state()) {
             std::string mode_text = get_modus_text_(static_cast<int>(val));
             this->defer([this, mode_text]() {
                 modus_brauchwasser_->publish_state(mode_text);
