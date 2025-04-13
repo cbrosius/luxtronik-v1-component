e77de4f1133bb2a95b03b039d9a998c9d03d2094
Christian Brosius <Christian.Brosius@gmail.com>
2025-04-13 15:03:17 +0200

    fix7

--- components/luxtronik_v1_component/luxtronik_v1_component.cpp
+++ components/luxtronik_v1_component/luxtronik_v1_component.cpp
@@ -231,8 +231,14 @@ void LuxtronikV1Component::parse_temperatur_message_(const char* message) {
         float temp = get_float_temp_(values[brauchwasser_soll_idx]);
         
         if (!brauchwasser_temperatur_number_->is_initialized()) {
+            // Initial state update
             brauchwasser_temperatur_number_->control(temp);
             brauchwasser_temperatur_number_->set_initialized(true);
+        } else {
+            // Update if value has changed (with small threshold to avoid floating point issues)
+            if (std::abs(brauchwasser_temperatur_number_->state - temp) > 0.1f) {
+                brauchwasser_temperatur_number_->publish_state(temp);
+            }
         }
     }
 
