fd67bbbb2a36f2e9e4c44dd0df2f9c5f0155eb9c
Christian Brosius <Christian.Brosius@gmail.com>
2025-04-13 23:10:12 +0200

    fix1

--- components/luxtronik_v1_component/luxtronik_v1_component.h
+++ components/luxtronik_v1_component/luxtronik_v1_component.h
@@ -5,6 +5,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/select/select.h"
+#include "esphome/components/number/number.h"
 
 namespace esphome {
 namespace luxtronik_v1_component {
