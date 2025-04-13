7f84b50255f930b6f7e7ecf7245884ad7f1b4ae7
Christian Brosius <Christian.Brosius@gmail.com>
2025-04-13 17:54:09 +0200

    fix48

--- components/luxtronik_v1_component/__init__.py
+++ components/luxtronik_v1_component/__init__.py
@@ -95,8 +95,6 @@ CONF_MISCHKREIS1_PARALLELVERSCHIEBUNG = "mischkreis1_parallelverschiebung"
 CONF_MISCHKREIS1_ABSENKUNG = "mischkreis1_absenkung"
 CONF_MISCHKREIS1_FESTWERT_VORLAUF = "mischkreis1_festwert_vorlauf"
 
-CONF_BRAUCHWASSER_TEMPERATUR_NUMBER = "brauchwasser_temperatur_number"
-
 MODUS_BRAUCHWASSER_OPTIONS = {
     "Automatik": 0,
     "Zweiter Waermeerzeuger": 1, 
@@ -194,12 +192,9 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_ID): cv.declare_id(ModusBrauchwasserSelect),
             cv.Optional(CONF_NAME): cv.string,
         }).extend(cv.COMPONENT_SCHEMA),
-        cv.Optional(CONF_BRAUCHWASSER_TEMPERATUR_NUMBER): number.NUMBER_SCHEMA.extend({
+        cv.Optional("brauchwasser_temperatur_number"): cv.Schema({
             cv.Required(CONF_ID): cv.declare_id(BrauchwasserTemperaturNumber),
-            cv.Optional(CONF_NAME): cv.string,
-            cv.Optional("min_value", default=40): cv.float_,
-            cv.Optional("max_value", default=75): cv.float_,
-            cv.Optional("step", default=1): cv.float_,
+            cv.Optional(CONF_NAME): cv.number,
         }).extend(cv.COMPONENT_SCHEMA),
 
         # Status sensors
@@ -417,8 +412,8 @@ async def to_code(config):
         )
         cg.add(var.set_modus_brauchwasser_select(var_select))
 
-    if CONF_BRAUCHWASSER_TEMPERATUR_NUMBER in config:
-        conf = config[CONF_BRAUCHWASSER_TEMPERATUR_NUMBER]
+    if "brauchwasser_temperatur_number" in config:
+        conf = config["brauchwasser_temperatur_number"]
         temp_number = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(temp_number, conf)
         await number.register_number(
