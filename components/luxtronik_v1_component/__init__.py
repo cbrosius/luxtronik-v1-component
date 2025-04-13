7aa6226007e449de91cacc9883b9b193c91ea036
Christian Brosius <Christian.Brosius@gmail.com>
2025-04-13 18:02:58 +0200

    fix52

--- components/luxtronik_v1_component/__init__.py
+++ components/luxtronik_v1_component/__init__.py
@@ -192,13 +192,13 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_ID): cv.declare_id(ModusBrauchwasserSelect),
             cv.Optional(CONF_NAME): cv.string,
         }).extend(cv.COMPONENT_SCHEMA),
-        cv.Optional("brauchwasser_temperatur_number"): cv.Schema({
-            cv.GenerateID(): cv.declare_id("BrauchwasserTemperaturNumber"),
+        cv.Optional("brauchwasser_temperatur_number"): number.NUMBER_SCHEMA.extend({
+            cv.GenerateID(): cv.declare_id(BrauchwasserTemperaturNumber),  # Changed from string to proper class
             cv.Optional(CONF_NAME): cv.string,
             cv.Optional("min_value", default=40): cv.float_,
             cv.Optional("max_value", default=75): cv.float_,
             cv.Optional("step", default=1): cv.float_,
-        }).extend(cv.COMPONENT_SCHEMA).extend(number.NUMBER_SCHEMA),
+        }).extend(cv.COMPONENT_SCHEMA),
 
         # Status sensors
         cv.Optional(CONF_STATUS_ANLAGENTYP): INPUT_OUTPUT_SCHEMA,
@@ -417,7 +417,7 @@ async def to_code(config):
 
     if "brauchwasser_temperatur_number" in config:
         conf = config["brauchwasser_temperatur_number"]
-        var_number = cg.Pvariable(conf[CONF_ID], LuxtronikV1Component.BrauchwasserTemperaturNumber)
+        var_number = await cg.new_Pvariable(conf[CONF_ID])  # Changed to await new_Pvariable
         await cg.register_component(var_number, conf)
         await number.register_number(
             var_number,
