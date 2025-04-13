5967426b6fec0680fcbd2f92e362367bef330806
Christian Brosius <Christian.Brosius@gmail.com>
2025-04-13 16:38:52 +0200

    fix28

--- components/luxtronik_v1_component/luxtronik_v1_component.h
+++ components/luxtronik_v1_component/luxtronik_v1_component.h
@@ -241,18 +241,21 @@ class BrauchwasserTemperaturNumber : public number::Number, public Component {
     if (std::abs(this->state - value) > 0.1f) {
       ESP_LOGD("BrauchwasserTemp", "Setting new temperature: %.1f", value);
       
-      if (parent_ != nullptr) {
-        // Format command: 3501;1;<temp*10>
-        char command[32];
-        snprintf(command, sizeof(command), "3501;1;%d\r\n", (int)(value * 10));
-        parent_->write_str(command);
-        
-        delay(100);  // Wait for command to be processed
-        
-        // Send save command
-        parent_->write_str("999\r\n");
-      }
+     // Check if UART is available
+     if (available()) {
+      // Format command: 3501;1;<temp*10>
+      char command[32];
+      snprintf(command, sizeof(command), "3501;1;%d\r\n", (int)(value * 10));
+      parent_->write_str(command);
       
+      delay(100);  // Wait for command to be processed
+      
+      // Send save command
+      parent_->write_str("999\r\n");
+    } else {
+      ESP_LOGW("BrauchwasserTemp", "UART not available");
+    }
+    
       // Update state after sending command
       this->publish_state(value);
     }
